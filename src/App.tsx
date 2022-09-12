7bdb46d9ea458896e0c2872b4fe537cd94b33468
DouglasWenzler <douglas.wenzler@hogarthww.com>
2022-09-12 15:37:52 -0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+interface ButtonProps {
+  title : string;
+}
+
+function Button (props : ButtonProps) {
+  return (
+    <button>
+      {props.title}
+    </button>
+  )
+}
+
 function App() {
-  return <h1>Hello NLW!</h1>
+  return (
+    <div>
+      <Button title="Send 1"/>
+      <Button title="Send 2"/>
+      <Button title="Send 3"/>
+      <Button title="Hello World!"/>
+    </div>
+  )
 }
 
 export default App
